e9336110f4dc3f9840166c3201fd09cb96e3f10f
VrnkProg1 <vlasova_nika69@mail.ru>
2024-10-24 04:31:34 +0300

    add html with css styles

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask import Flask
 from dotenv import load_dotenv
 import os
@@ -6,6 +7,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
