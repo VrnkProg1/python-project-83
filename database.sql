256edcdc664292643166f3f7e40e5c01e6779dda
VrnkProg1 <vlasova_nika69@mail.ru>
2025-01-12 00:34:23 +0300

    update sql file

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
 id SERIAL PRIMARY KEY,
 name VARCHAR(255) NOT NULL,
 created_at DATE
 );
 
-CREATE TABLE url_checks (
+CREATE TABLE IF NOT EXISTS url_checks (
 id SERIAL PRIMARY KEY,
 url_id INT,
 status_code INT,
